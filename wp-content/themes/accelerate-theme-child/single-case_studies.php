d25696d63e1cdaf7ef3af1385d61e49cd331bf15
fstacker <brett@yutster.com>
2018-09-03 21:10:03 -0600

    in the middle of second single-case-studies redo

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying a single case study
+ * The template for displaying all pages
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -14,50 +14,39 @@
 
 get_header(); ?>
 
-<div id="primary" class="site-content">
-	<div class="main-content" role="main">
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); 
+				$services = get_field('services');
+				$client = get_field('client') 
+				$image_1 = get_field('image_1')
+				$image_2 = get_field('image_2')
+				$image_3 = get_field('image_3')
+				?>
 
+		<article class="case-study">
+			<aside class="case-study-sidebar">
+				<h2><?php the_title(); ?></h2>
+				<h4><?php echo $services;?></h4>
 
-	<?php while ( have_posts() ) : the_post();
-		$services = get_field('services');
-		$client = get_field('client');
-		$link = get_field('site_link');
-		$image_1 = get_field('image_1');
-		$image_2 = get_field('image_2');
-		$image_3 = get_field('image_3');
-		$size = 'full';
-		?>
+				<?php the_content(); ?>
 
-	<article class="case-study">
-		<aside class="case-study-sidebar">
-			<h2><?php the_title(); ?></h2>
-			<h5><?php echo $services; ?></h5>
-			<h6><span>Client:  <?php echo $client; ?></span></h6>
+				<p class="read-more-link"><a href="<?php echo $link; ?>">Vist Live Site ></a></p>
+			</aside>
 
-			<?php the_content(); ?>
-			
-			<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Site &rsaquo;</a></p>
-		</aside>
+			<div class="case-study-images">
+				<img src="<?php echo $image_1 ?>" /></img>
 
-		<div class="case-study-images">
-			<?php if($image_1) {
-				echo wp_get_attachment_image($image_1, $size);
-			} ?>
-			
-			<?php if($image_2) { ?
-				echo wp_get_attachment_image($image_2, $size);
-			} ?>
+				<img src="<?php echo $image_2 ?>" /></img>
 
-			<?php if($image_3) { ?
-				echo wp_get_attachment_image($image_3, $size);
-			} ?>
-			
-		</div>
-	</article>
+				<img src="<?php echo $image_3 ?>" /></img>
+			</div>
 
-	<?php endwhile; // end of the loop. ?>
-	</div> <!--.main-content --> 
+		</article>
 
-</div> <!-- #primary -->
+				
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>
