7ef6ea36d821e13153dd9e7c85f972285b3e725f
fstacker <brett@yutster.com>
2018-09-28 11:15:17 -0600

    page-about query added; not working

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -14,44 +14,34 @@ get_header(); ?>
 			<div class="about-description clearfix">
 				<p>Accelerate is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile. </p>
 			</div>				
-				<?php // Start the loop
-					while ( have_posts() ) : the_post();
-						$type = get_field('type');
-						$description = get_field('description');
-						$icon = get_field('icon');
-						$size = 'medium'
-					?>
-					<?php //the_content(); ?>
-				<?php endwhile; // end of the loop. ?>
-		
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
 	<div class="about-services clearfix">
 		<div class="services">
-			<h4><?php the_title(); ?></h4>
-			<p><?php the_content(); ?></p>
+			<ul>
+				<?php new WP_Query('page=services'); ?>
+					<?php // Start the loop
+						while ( have_posts() ) : the_post();
+							$type = get_field('type');
+							$description = get_field('description');
+							$icon = get_field('icon');
+							$size = 'full';
+						?>
+						<h4><?php the_title(); ?></h4>
+						<p><?php the_content(); ?></p>
+
+						<h3><?php echo $type; ?></h3>
+						<p><?php echo $description; ?></p>
+						<figure>
+							<?php echo wp_get_attachment_image($image, $size); ?>
+						</figure>
+					<?php endwhile; // end of the loop ?>
+				<?php wp_reset_query(); ?>
+			</ul>	
 		</div>
 	</div>
 
-	<ul class="services-list">
-		<?php query_posts('posts_per_page=4&post_type=services'); ?>
-			<?php while (have_posts() ) : the_post();
-				$icon = get_field('icon');
-				$description = get_field('description');
-				$size = "medium";
-			?>
-				<li class="service-list-item"> 	
-						<h3><?php the_title(); ?></h3>
-						<p><?php echo $description; ?></p>
-						<figure>
-							<?php echo wp_get_attachment_image($icon, $size); ?>
-						</figure>
-						
-				</li>
-			<?php endwhile; ?>
-		<?php wp_reset_query(); ?>
-	</ul>
 
 
 	<div class="interested-contact clearfix">
